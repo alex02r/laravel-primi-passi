8975956f3fc5c344ff38604224f2bc317db47bbb
alex02r <alex.giochi02@gmail.com>
2024-02-09 15:02:05 +0100

    [UPGRADE] Passaggio di valori tramite route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $list = ['home', 'img', 'about'];
+    return view('homepage', compact('list'));
 });
